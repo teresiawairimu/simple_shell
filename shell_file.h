9001b9f43e60a82725299d2ab150f474950fd2ee
teresiawairimu <nduatiteresia82@gmail.com>
2023-12-14 13:01:40 -0800

    shell_file.h

--- shell_file.h
+++ shell_file.h
@@ -2,6 +2,8 @@
 #define SELL_FILE_H
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -9,5 +11,7 @@
 
 void string_print(const char *text);
 void render_prompt();
+void carry_out_command(const char *instruction);
+void fetch_user_command(char *instruction, size_t size);
 
 #endif /* SHELL_FILE_H*/
