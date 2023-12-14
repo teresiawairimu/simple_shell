1bf9ec95c95196e194f5eb3e7de106660f8c33d0
teresiawairimu <nduatiteresia82@gmail.com>
2023-12-14 12:09:52 -0800

    Added the header file

--- shell_file.h
+++ shell_file.h
@@ -1,4 +1,13 @@
 #ifndef SHELL_FILE_H
-#define SHELL_FILE_H
+#define SELL_FILE_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+void string_print(const char *text);
+void render_prompt();
 
 #endif /* SHELL_FILE_H*/
